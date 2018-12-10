d5e9daede3604e22389530cf71f49c96e6efde7c
naelah <naelahnordin@gmail.com>
2018-12-10 10:38:56 +0900

    ammended tables

--- others/two/vendor/jquery/jquery.js
+++ others/two/vendor/jquery/jquery.js
@@ -4684,10 +4684,10 @@ var wrapMap = {
 	// XHTML parsers do not magically insert elements in the
 	// same way that tag soup parsers do. So we cannot shorten
 	// this by omitting <tbody> or other required elements.
-	thead: [ 1, "<table>", "</table>" ],
-	col: [ 2, "<table><colgroup>", "</colgroup></table>" ],
-	tr: [ 2, "<table><tbody>", "</tbody></table>" ],
-	td: [ 3, "<table><tbody><tr>", "</tr></tbody></table>" ],
+	thead: [ 1, "<table>", "</table></div>" ],
+	col: [ 2, "<table><colgroup>", "</colgroup></table></div>" ],
+	tr: [ 2, "<table><tbody>", "</tbody></table></div>" ],
+	td: [ 3, "<table><tbody><tr>", "</tr></tbody></table></div>" ],
 
 	_default: [ 0, "", "" ]
 };
